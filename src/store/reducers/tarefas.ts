380d250ce335a2f8704baab4e3b62568506ddf2b
rodrigopenafurt <rodfpar@hotmail.com>
2024-04-08 08:17:07 -0300

    resultado da filtragem

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -37,33 +37,22 @@ const tarefasSlice = createSlice({
   initialState,
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
-      state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
+      state.itens = [
+        ...state.itens.filter((tarefa) => tarefa.id !== action.payload)
+      ]
+    },
+    editar: (state, action: PayloadAction<Tarefa>) => {
+      const indexDaTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+
+      if (indexDaTarefa >= 0) {
+        state.itens[indexDaTarefa] = action.payload
+      }
     }
   }
 })
 
-export const { remover } = tarefasSlice.actions
+export const { remover, editar } = tarefasSlice.actions
 
 export default tarefasSlice.reducer
-
-new Tarefa(
-  'Estudar Javascript',
-  enums.Prioridade.IMPORTANTE,
-  enums.Status.PENDENTE,
-  '',
-  1
-),
-  new Tarefa(
-    'Estudar Typescript',
-    enums.Prioridade.URGENTE,
-    enums.Status.PENDENTE,
-    'Rever aula 2 do módulo',
-    2
-  ),
-  new Tarefa(
-    'Estudar React',
-    enums.Prioridade.URGENTE,
-    enums.Status.PENDENTE,
-    'Praticar o useEffect',
-    3
-  )
